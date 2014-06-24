dbee77d8e5e1532c389e0ed2a804785e4997b9dc
Ian <irstevenson@gmail.com>
2014-06-24 22:06:47 +1000

    Adding codenarc quality checking support to the build

---
+++ config/codenarc/rules.groovy
@@ -0,0 +1,8 @@
+ruleset {
+	description 'CodeNarc Rules'
+
+	ruleset('rulesets/basic.xml')
+	ruleset('rulesets/imports.xml')
+	ruleset('rulesets/unnecessary.xml')
+	ruleset('rulesets/unused.xml')
+}
